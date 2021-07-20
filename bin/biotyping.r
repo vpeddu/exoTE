25ed56963c9284caa3016cf070613c4ab1255fdb
vikas peddu <vikas.peddu@gmail.com>
2021-07-20 16:14:30 -0700

    read length boxplots

--- bin/biotyping.r
+++ bin/biotyping.r
@@ -5,7 +5,13 @@ library(svMisc)
 library(tidyverse)
 library(plyr)
 
+#read bedfile
+print('reading bed file')
 bedfile <-import("/Users/vikas/Documents/UCSC/lab/kim_lab/exosomal_rna/testing/out.bed", format="bed")
+
+#read bamfile
+print('reading bamfile')
+bam <- scanBam('/Users/vikas/Documents/UCSC/lab/kim_lab/exosomal_rna/testing/05_25_21_R941_CTRL_EXOSOME_CDNA_Guppy_5.0.7_sup.minimap2.sorted.bam')
 
 df <- data.frame(readname = bedfile$name, bed_info = bedfile$NA.8)
 df$biotype<-gsub('\\"','',  df$bed_info)
@@ -40,3 +46,23 @@ biotype_plot<-ggplot(plot_df, aes(x = Category, y = x, fill = Category)) +
   scale_fill_viridis_d() 
 biotype_plot
 
+#Pulling read length by biotype
+bam_df<-do.call("DataFrame", bam)
+bam_indices<-which(bam_df$qname %in% aggregated$Var1)
+bam_biotyped<-as.data.frame(bam_df[bam_indices,])
+bam_biotyped<-bam_biotyped[!duplicated(bam_biotyped$qname),]
+
+#merging biotyping and read lengths
+biotyped_rl<-full_join(bam_biotyped, aggregated, by = c("qname" = "Var1"))
+
+biotype_rl_plot<-ggplot(biotyped_rl, aes(x = Var2, y = qwidth)) + 
+  geom_boxplot() +
+  theme_classic() + 
+  #scale_y_log10(limits = c(1,1e7)) +
+  scale_y_log10(limits = c(10,10000), expand = c(0, 0)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1), 
+        legend.position = 'none') +
+  ylab("Read length") + 
+  xlab('Biotype') +
+  scale_fill_viridis_d() 
+biotype_rl_plot
